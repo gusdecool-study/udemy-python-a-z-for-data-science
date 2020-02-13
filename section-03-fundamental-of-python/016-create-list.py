81d952534e3286354ba91db2b22aeafbb4c08290
Budi Arsana <gusdecool@gmail.com>
2020-02-14 00:05:00 +0800

    017 using brackets

---
+++ section-03-fundamental-of-python/016-create-list.py
@@ -0,0 +1,6 @@
+MyFirstList = [3, 45, 56, 732]
+type(MyFirstList)
+
+Foo = ['hello', 24, True, 55.3]
+Foo1 = range(1, 8)
+Foo2 = list(range(1, 8))
---
+++ section-03-fundamental-of-python/017-using-brackets.py
@@ -0,0 +1,5 @@
+w = ['a', 'b', 'c', 'd', 'e']
+
+print(w[0])  # a
+print(len(w))  # 5
+print(w[-1])  # e
