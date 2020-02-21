f97ad0822c6b0f2921d8090c4a5c2996b4814617
Budi Arsana <gusdecool@gmail.com>
2020-02-21 23:29:58 +0800

    038 importing data

--- section-05-data-frames/038-importing-data.py
+++ section-05-data-frames/038-importing-data.py
@@ -5,3 +5,25 @@ cwd = os.getcwd()
 file = '/section-05-data-frames/00-demographic-data.csv'
 
 csv = pd.read_csv(cwd + file)
+
+# 2. number of rows
+print(len(csv))
+
+# 3. see columns
+print(csv.columns)
+
+# 4. number of columns
+print(len(csv.columns))
+
+# 5. top rows
+print(csv.head())
+
+# 6. bottom rows
+print(csv.tail())
+
+# 7. information on the columns
+print(csv.info())
+
+# 8. get stats of the columns
+print(csv.describe())
+print(csv.describe().transpose())
