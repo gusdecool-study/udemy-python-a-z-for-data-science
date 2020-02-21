8094ae34c2b077a1d3af1aa44d7cd3602d89979b
Budi Arsana <gusdecool@gmail.com>
2020-02-21 22:34:15 +0800

    034 advanced function

--- section-04-matrices/033-function.py
+++ section-04-matrices/033-function.py
@@ -1,14 +1,22 @@
 import matplotlib.pyplot as plt
 
 
-def myplot(playerList):
+# Run the code in 027-matrices.py first
+def myplot(data, playerList = Players):
+    color = {'KobeBryant': 'Black', 'JoeJohnson': 'Red', 'LeBronJames': 'Green', 'CarmeloAnthony': 'Blue',
+             'DwightHoward': 'Magenta', 'ChrisBosh': 'Black', 'ChrisPaul': 'Red', 'KevinDurant': 'Green',
+             'DerrickRose': 'Blue', 'DwayneWade': 'Magenta'}
+
+    marker = {'KobeBryant': 's', 'JoeJohnson': 'o', 'LeBronJames': '^', 'CarmeloAnthony': 's',
+              'DwightHoward': 's', 'ChrisBosh': 'o', 'ChrisPaul': '^', 'KevinDurant': 'D',
+              'DerrickRose': 's', 'DwayneWade': 'o'}
+
     for name in playerList:
-        plt.plot(Games[Pdict[name]], c='Black', ls='--', marker='s', ms=7, label=name)
+        plt.plot(data[Pdict[name]], c=color[name], ls='--', marker=marker[name], ms=7, label=name)
 
     plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
     plt.xticks(list(range(0, 10)), Seasons, rotation='vertical')
     plt.show()
 
 
-myplot(['KobeBryant', 'JoeJohnson', 'LeBronJames', 'CarmeloAnthony', 'DwightHoward', 'ChrisBosh', 'ChrisPaul',
-        'KevinDurant', 'DerrickRose', 'DwayneWade'])
+myplot(MinutesPlayed)
